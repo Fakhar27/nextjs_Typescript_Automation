a829b1a4190c8bbe842774231fe0ae055ea12da2
Fakhar27 <fakharmursaleen12345@gmail.com>
2024-10-19 01:44:42 +0500

    UPDATING AT 19TH OCT 2AM

---
+++ automation/src/auth.ts
@@ -0,0 +1,29 @@
+import NextAuth from "next-auth"
+import Google from "next-auth/providers/google"
+import GitHub from "next-auth/providers/github"
+import Credentials from "next-auth/providers/credentials"
+ 
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      }}),
+  ],
+})
